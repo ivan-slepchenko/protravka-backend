ec5265121c58891fb625ded81c4bf0cba8feedc7
ivansliepchenko <ivan.slepchenko@zettech.com>
2024-11-27 17:08:41 +0100

    Refactor Firebase configuration to use environment variables and remove service account key file

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,17 @@
 import firebase from "firebase/app";
-import serviceAccount from "./serviceAccountKey.json";
-import admin from 'firebase-admin';
+import admin, { ServiceAccount } from 'firebase-admin';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const serviceAccount: ServiceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 export const adminApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const firebaseConfig = {
