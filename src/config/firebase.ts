9879766b094b9f977a0df61f9bd29a7121333a9f
Ivan Sliepchenko <ivan.slepchenko@zettech.com>
2025-03-04 22:35:37 +0100

    fix: Refactor Firebase configuration and middleware for consistency

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,5 @@
-import * as firebase from "firebase/app";
-import admin, { initializeApp, ServiceAccount } from 'firebase-admin';
+import * as firebase from 'firebase/app';
+import admin, { ServiceAccount } from 'firebase-admin';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,17 +10,17 @@ const firebaseConfig = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-  };
-  
+    appId: process.env.FIREBASE_APP_ID,
+};
+
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const serviceAccount: ServiceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
 export const firebaseAdminApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,24 @@
-import { NextFunction, Request, Response } from "express";
-import { firebaseAdminApp } from "./config/firebase";
+import { NextFunction, Request, Response } from 'express';
+import { firebaseAdminApp } from './config/firebase';
 
 declare module 'express-serve-static-core' {
-  interface Request {
-    user?: any;
-  }
+    interface Request {
+        user?: any;
+    }
 }
 
 export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
-  const idToken = req.cookies.access_token;
-  if (!idToken) {
-    res.status(403).json({ error: 'No token provided' });
-  } else {
-    try {
-        const decodedToken = await firebaseAdminApp.auth().verifyIdToken(idToken);
-        req.user = decodedToken;
-        next();
-      } catch (error) {
-        console.error('Error verifying token:', error);
-        res.status(403).json({ error: 'Unauthorized' });
-      }
-  }
+    const idToken = req.cookies.access_token;
+    if (!idToken) {
+        res.status(403).json({ error: 'No token provided' });
+    } else {
+        try {
+            const decodedToken = await firebaseAdminApp.auth().verifyIdToken(idToken);
+            req.user = decodedToken;
+            next();
+        } catch (error) {
+            console.error('Error verifying token:', error);
+            res.status(403).json({ error: 'Unauthorized' });
+        }
+    }
 };
